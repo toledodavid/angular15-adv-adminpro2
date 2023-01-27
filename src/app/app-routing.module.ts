a2445314f883cd9301323257268a7e0ce7e7a6de
David Toledo <dtoledoz0207@gmail.com>
2023-01-27 10:58:57 -0600

    Create Pages component and implement secondary routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,16 +5,25 @@ import { RegisterComponent } from "./auth/register/register.component";
 import { DashboardComponent } from "./pages/dashboard/dashboard.component";
 import { Graph1Component } from "./pages/graph1/graph1.component";
 import { NotpagefoundComponent } from "./pages/notpagefound/notpagefound.component";
+import { PagesComponent } from "./pages/pages.component";
 import { ProgressComponent } from "./pages/progress/progress.component";
 
 
 const routes: Routes = [
-  { path: 'dashboard', component: DashboardComponent },
+  {
+    path: '',
+    component: PagesComponent,
+    children: [
+      { path: 'dashboard', component: DashboardComponent },
+      { path: 'progress', component: ProgressComponent },
+      { path: 'graph1', component: Graph1Component },
+      { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+    ]
+  },
+
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
-  { path: 'progress', component: ProgressComponent },
-  { path: 'graph1', component: Graph1Component },
-  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+
   { path: '**', component: NotpagefoundComponent },
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,6 +13,7 @@ import { SidebarComponent } from './shared/sidebar/sidebar.component';
 import { HeaderComponent } from './shared/header/header.component';
 import { ProgressComponent } from './pages/progress/progress.component';
 import { Graph1Component } from './pages/graph1/graph1.component';
+import { PagesComponent } from './pages/pages.component';
 
 @NgModule({
   declarations: [
@@ -25,7 +26,8 @@ import { Graph1Component } from './pages/graph1/graph1.component';
     SidebarComponent,
     HeaderComponent,
     ProgressComponent,
-    Graph1Component
+    Graph1Component,
+    PagesComponent
   ],
   imports: [
     BrowserModule,
