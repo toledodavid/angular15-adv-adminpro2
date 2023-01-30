3e0fc355d6f771dea6c7acbe8d5990bb95269170
David Toledo <dtoledoz0207@gmail.com>
2023-01-30 10:49:02 -0600

    Create PagesModule and SharedModule

---
+++ src/app/pages/pages.module.ts
@@ -0,0 +1,32 @@
+import { CommonModule } from "@angular/common";
+import { NgModule } from "@angular/core";
+import { RouterModule } from "@angular/router";
+import { SharedModule } from "../shared/shared.module";
+
+import { DashboardComponent } from "./dashboard/dashboard.component";
+import { Graph1Component } from "./graph1/graph1.component";
+import { PagesComponent } from "./pages.component";
+import { ProgressComponent } from "./progress/progress.component";
+
+
+
+@NgModule({
+  declarations: [
+    DashboardComponent,
+    ProgressComponent,
+    Graph1Component,
+    PagesComponent
+  ],
+  exports: [
+    DashboardComponent,
+    ProgressComponent,
+    Graph1Component,
+    PagesComponent
+  ],
+  imports: [
+    CommonModule,
+    SharedModule,
+    RouterModule
+  ]
+})
+export class PagesModule {}
---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,21 @@
+import { CommonModule } from "@angular/common";
+import { NgModule } from "@angular/core";
+import { BreadcrumbsComponent } from "./breadcrumbs/breadcrumbs.component";
+import { HeaderComponent } from "./header/header.component";
+import { SidebarComponent } from "./sidebar/sidebar.component";
+
+
+@NgModule({
+  declarations: [
+    BreadcrumbsComponent,
+    SidebarComponent,
+    HeaderComponent,
+  ],
+  exports: [
+    BreadcrumbsComponent,
+    SidebarComponent,
+    HeaderComponent,
+  ],
+  imports: [ CommonModule ]
+})
+export class SharedModule {}
