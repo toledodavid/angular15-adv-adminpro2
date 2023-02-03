98f55bf22e215b9cfc3b9298f234f6763888f4e4
David Toledo <dtoledoz0207@gmail.com>
2023-02-03 17:01:33 -0600

    Install ng2-chart and chart.js libraries and create chart in Graph1 Component

--- src/app/pages/graph1/graph1.component.ts
+++ src/app/pages/graph1/graph1.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 
+import { ChartData, Color } from 'chart.js';
+
 @Component({
   selector: 'app-graph1',
   templateUrl: './graph1.component.html',
@@ -7,5 +9,19 @@ import { Component } from '@angular/core';
   ]
 })
 export class Graph1Component {
+
+  // Doughnut
+  public doughnutChartLabels: string[] = [ 'Download Sales', 'In-Store Sales', 'Mail-Order Sales' ];
+  public colors: string[] = ['#6857E6', '#009FEE', '#F02059'];
+
+  public doughnutChartData: ChartData<'doughnut'> = {
+    labels: this.doughnutChartLabels,
+    datasets: [
+      {
+        data: [ 350, 450, 100 ],
+        backgroundColor: this.colors,
+      }
+    ]
+  };
 
 }
--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,7 +1,9 @@
 import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
-
 import { RouterModule } from "@angular/router";
+
+import { NgChartsModule } from 'ng2-charts';
+
 import { ComponentsModule } from "../components/components.module";
 import { SharedModule } from "../shared/shared.module";
 
@@ -23,7 +25,8 @@ import { ProgressComponent } from "./progress/progress.component";
     CommonModule,
     SharedModule,
     RouterModule,
-    ComponentsModule
+    ComponentsModule,
+    NgChartsModule,
   ]
 })
 export class PagesModule {}
