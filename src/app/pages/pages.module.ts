2e4a46289ab70bf49b7de41b60ce15fe09c4a4cd
David Toledo <dtoledoz0207@gmail.com>
2023-02-08 11:03:54 -0600

    Create AccountSettings Component and create its design

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -9,6 +9,7 @@ import { DashboardComponent } from "./dashboard/dashboard.component";
 import { Graph1Component } from "./graph1/graph1.component";
 import { PagesComponent } from "./pages.component";
 import { ProgressComponent } from "./progress/progress.component";
+import { AccountSettings } from "./account-settings/account-settings.component";
 
 
 
@@ -17,7 +18,8 @@ import { ProgressComponent } from "./progress/progress.component";
     DashboardComponent,
     ProgressComponent,
     Graph1Component,
-    PagesComponent
+    PagesComponent,
+    AccountSettings
   ],
   imports: [
     CommonModule,
--- src/app/pages/pages.routing.ts
+++ src/app/pages/pages.routing.ts
@@ -1,5 +1,6 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
+import { AccountSettings } from "./account-settings/account-settings.component";
 import { DashboardComponent } from "./dashboard/dashboard.component";
 import { Graph1Component } from "./graph1/graph1.component";
 import { PagesComponent } from "./pages.component";
@@ -13,6 +14,7 @@ const routes: Routes = [
       { path: '', component: DashboardComponent },
       { path: 'progress', component: ProgressComponent },
       { path: 'graph1', component: Graph1Component },
+      { path: 'account-settings', component: AccountSettings }
     ]
   },
 ];
