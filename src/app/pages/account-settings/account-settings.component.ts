660ac3c4946c842589be35f6d727c1f7e7a0cc6f
David Toledo <dtoledoz0207@gmail.com>
2023-02-10 10:30:20 -0600

    Add 'working' class to HTML element from AccountSettings Component

--- src/app/pages/account-settings/account-settings.component.ts
+++ src/app/pages/account-settings/account-settings.component.ts
@@ -1,4 +1,4 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 
 
 
@@ -7,14 +7,35 @@ import { Component } from "@angular/core";
   templateUrl: './account-settings.component.html',
   styles: []
 })
-export class AccountSettings {
+export class AccountSettings implements OnInit {
 
   linkTheme = document.querySelector('#theme');
+  public links: any;
+
+  ngOnInit(): void {
+    this.links = document.querySelectorAll('.selector');
+    this.checkCurrentTheme();
+  }
 
   changeTheme(theme: string) {
     const url = `./assets/css/colors/${theme}.css`;
     this.linkTheme?.setAttribute('href', url);
     localStorage.setItem('theme', url);
+
+    this.checkCurrentTheme();
+  }
+
+  checkCurrentTheme() {
+    this.links.forEach((element:any) => {
+      element.classList.remove('working');
+      const btnTheme = element.getAttribute('data-theme');
+      const btnThemeUrl = `./assets/css/colors/${btnTheme}.css`;
+      const currentThemeUrl = this.linkTheme?.getAttribute('href');
+
+      if (btnThemeUrl === currentThemeUrl) {
+        element.classList.add('working');
+      }
+    });
   }
 
 }
