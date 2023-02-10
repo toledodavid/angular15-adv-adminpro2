09c7a5f951a896dcfb52b1aad7bc4b4270a4a4cf
David Toledo <dtoledoz0207@gmail.com>
2023-02-10 11:16:54 -0600

    Add routerLink in Sidebar and Header Components to navigate to account-settings path

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,5 +1,7 @@
 import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
+import { RouterModule } from "@angular/router";
+
 import { BreadcrumbsComponent } from "./breadcrumbs/breadcrumbs.component";
 import { HeaderComponent } from "./header/header.component";
 import { SidebarComponent } from "./sidebar/sidebar.component";
@@ -16,6 +18,6 @@ import { SidebarComponent } from "./sidebar/sidebar.component";
     SidebarComponent,
     HeaderComponent,
   ],
-  imports: [ CommonModule ]
+  imports: [ CommonModule, RouterModule ]
 })
 export class SharedModule {}
