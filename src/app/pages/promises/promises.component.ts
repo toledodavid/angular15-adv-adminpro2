cf3c08ea1dae911885133f186a8afc695936062a
David Toledo <dtoledoz0207@gmail.com>
2023-02-21 11:25:28 -0600

    Add fetch method in Promises Component to get users from reqres.in api

--- src/app/pages/promises/promises.component.ts
+++ src/app/pages/promises/promises.component.ts
@@ -9,24 +9,33 @@ import { Component, OnInit } from "@angular/core";
 export class PromisesComponent implements OnInit {
 
   ngOnInit(): void {
-    const promise = new Promise((resolve, reject) => {
-      console.log('Hello world');
+    // const promise = new Promise((resolve, reject) => {
+    //   console.log('Hello world');
+    //   if (false) {
+    //     resolve('Future then...');
+    //   } else {
+    //     reject('Something is wrong');
+    //   }
+    // });
 
-      if (false) {
-        resolve('Future then...');
-      } else {
-        reject('Something is wrong');
-      }
+    // promise.then((message) => {
+    //   console.log(message);
+    // }).catch(err => {
+    //   console.warn(err);
+    // });
 
+    // console.log('Init ends');
 
+    this.getUsers().then(users => {
+      console.log(users);
     });
+  }
 
-    promise.then((message) => {
-      console.log(message);
-    }).catch(err => {
-      console.warn(err);
+  getUsers() {
+    return new Promise(resolve => {
+      fetch('https://reqres.in/api/users')
+      .then(response => response.json())
+      .then(body => resolve(body.data));
     });
-
-    console.log('Init ends');
   }
 }
