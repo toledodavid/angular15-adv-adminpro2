e14570541c85ccd4d2b8370b8d57dabb095070b2
David Toledo <dtoledoz0207@gmail.com>
2023-02-27 11:01:41 -0600

    Create an observable in Rxjs Component

--- src/app/pages/rxjs/rxjs.component.ts
+++ src/app/pages/rxjs/rxjs.component.ts
@@ -1,4 +1,5 @@
 import { Component } from "@angular/core";
+import { Observable } from "rxjs";
 
 
 @Component({
@@ -7,5 +8,39 @@ import { Component } from "@angular/core";
   styleUrls: []
 })
 export class RxjsComponent {
+
+  constructor() {
+    const ob$ = new Observable(observer => {
+
+      let i = -1;
+
+      const interval = setInterval(() => {
+        i++;
+        observer.next(i);
+
+        if (i === 4) {
+          clearInterval(interval);
+          observer.complete();
+        }
+
+        if (i === 2) {
+          observer.error('i is 2');
+        }
+      }, 1000);
+    });
+
+    // -------- DEPRECATED -------------
+    // ob$.subscribe(
+    //   value => console.log('Subs: ', value),
+    //   error => console.warn(error),
+    //   () => console.info('Obs finished')
+    // );
+
+    ob$.subscribe({
+      next: value => console.log('Subs: ', value),
+      error: error => console.warn(error),
+      complete: () => console.info('Obs finished')
+    });
+  }
 
 }
