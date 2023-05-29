bb7c2d0b1b0e3f7fff120e8e5724c8cb20f51257
David Toledo <dtoledoz0207@gmail.com>
2023-05-29 10:15:43 -0600

    Add Maintenance menu in SidebarService

--- src/app/services/sidebar.service.ts
+++ src/app/services/sidebar.service.ts
@@ -17,6 +17,15 @@ export class SidebarService {
         { title: 'Promises', url: 'promises' },
         { title: 'Rxjs', url: 'rxjs' }
       ]
+    },
+    {
+      title: 'Maintenance',
+      icon: 'mdi mdi-folder-lock-open',
+      submenu: [
+        { title: 'Users', url: 'users' },
+        { title: 'Hospitals', url: 'hospitals' },
+        { title: 'Doctors', url: 'doctors' }
+      ]
     }
   ]
 
