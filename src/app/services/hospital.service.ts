64411b8d197f97fb8a8ea622fa7f0ccac7e98976
David Toledo <dtoledoz0207@gmail.com>
2023-06-19 11:37:31 -0600

    Create methods for createHospital, updateHospital and deleteHospital in HospitalService and add create hospital button in HospitalsComponent

--- src/app/services/hospital.service.ts
+++ src/app/services/hospital.service.ts
@@ -35,4 +35,19 @@ export class HospitalService {
     );
   }
 
+  createHospital(name: string) {
+    const url = `${base_url}/hospitals`;
+    return this.http.post(url, {name}, this.headers);
+  }
+
+  updateHospital(_id: string, name: string) {
+    const url = `${base_url}/hospitals/${_id}`;
+    return this.http.put(url, {name}, this.headers);
+  }
+
+  deleteHospital(_id: string) {
+    const url = `${base_url}/hospitals/${_id}`;
+    return this.http.delete(url, this.headers);
+  }
+
 }
