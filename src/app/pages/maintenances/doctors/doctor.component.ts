092b8f407f39b1c00e82b89d2b962f207dbb7d7c
David Toledo <dtoledoz0207@gmail.com>
2023-07-03 13:05:03 -0600

    Implement formGroup and formControl name attributes to handle form information in DoctorComponent and get hospitals to show them in select element

--- src/app/pages/maintenances/doctors/doctor.component.ts
+++ src/app/pages/maintenances/doctors/doctor.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { HospitalService } from '../../../services/hospital.service';
+import { Hospital } from 'src/app/models/hospital.model';
 
 
 @Component({
@@ -6,6 +9,32 @@ import { Component } from '@angular/core';
   templateUrl: './doctor.component.html',
   styleUrls: []
 })
-export class DoctorComponent {
-  constructor() {}
+export class DoctorComponent implements OnInit {
+
+  public doctorForm: FormGroup = this.formBuilder.group({
+    name: ['Javier', Validators.required],
+    hospital: ['', Validators.required]
+  });
+
+  hospitals?: Hospital [] = [];
+
+  constructor(private formBuilder: FormBuilder, private hospitalService: HospitalService) {}
+
+  ngOnInit(): void {
+    this.loadHospitals();
+  }
+
+  loadHospitals() {
+    this.hospitalService.loadHospitals().subscribe({
+      next: (hospitals: Hospital[]) => {
+        this.hospitals = hospitals;
+        console.log(this.hospitals);
+      }
+    });
+  }
+
+  saveDoctor() {
+    console.log(this.doctorForm.value);
+  }
+
 }
